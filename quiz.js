66b86e4e6f9e71740b1406a31158f1fe2d4163d9
NourGm28 <nournornor179@gmail.com>
2024-10-13 20:28:41 +0300

    getting back to the correct concept

--- quiz.js
+++ quiz.js
@@ -5,13 +5,12 @@ function checkAnswer(){
     
     const correctAnswer = "4";
     const form = document.getElementById("quiz-container");
-    let userAnswer =form.querySelector('input[name ="quiz"]:checked');
-    if(userAnswer) {
-        if (userAnswer.value === correctAnswer){
+    let userAnswer =form.querySelector('input[name ="quiz"]:checked').value;
+        if (userAnswer === correctAnswer){
         feedback.textContent = "Correct! Well done.";
     }else{
         feedback.textContent = "That's incorrect. Try again!";
-        }
+        
     }
 }
 let submit = document.getElementById('submit-answer');
